72fd39557a7da2a5f9dcd80f4281b34ad37889f6
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-15 17:57:23 -0400

    creates and sets up controllers/skills.js

---
+++ controllers/skills.js
@@ -0,0 +1,12 @@
+//import the Skills data
+import { skills } from '../data/skill-data.js'
+
+function index(req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
+}
+
+export {
+  index
+}
--- data/skill-data.js
+++ data/skill-data.js
@@ -1,7 +1,8 @@
 const skills = [
   {text: 'CSS', pro: true, _id: 125223},
-  {text: 'HTML', pro: false, _id: 127904},
-  {text: 'Javascript', pro: false, _id: 139608},
+  {text: 'HTML', pro: true, _id: 127904},
+  {text: 'Javascript', pro: true, _id: 139608},
+  {text: 'Guitar Player', pro: false, _id: 139602}
 ]
 
 export { 
